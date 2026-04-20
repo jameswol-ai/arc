a56104769fe2827124423e0aa048ca1bf1c02339
James Wol <james.wol@petalmail.com>
2026-04-20 19:58:23 +0200

    Create design_workflow.py

---
+++ workflows/design_workflow.py
@@ -0,0 +1,12 @@
+def design_concept(user_input):
+    return f"""
+    Generate an architectural concept based on:
+
+    {user_input}
+
+    Include:
+    - Design style
+    - Spatial layout
+    - Materials
+    - Environmental considerations
+    """
