0a85fc3c4c957eda26dcf67a0ee9e4d63b637d5f
James Wol <james.wol@petalmail.com>
2026-04-20 23:39:52 +0200

    Create context.py

---
+++ context.py
@@ -0,0 +1,17 @@
+# src/core/context.py
+
+class Context:
+    def __init__(self):
+        self.data = {}
+
+    def set(self, key, value):
+        self.data[key] = value
+
+    def get(self, key, default=None):
+        return self.data.get(key, default)
+
+    def update(self, new_data: dict):
+        self.data.update(new_data)
+
+    def all(self):
+        return self.data
