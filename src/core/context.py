5feb23bc3f3a3368e8856bfaf27daa16872a5606
James Wol <james.wol@petalmail.com>
2026-04-27 19:38:05 +0200

    Update context.py

--- src/core/context.py
+++ src/core/context.py
@@ -1,20 +1,13 @@
 # src/core/context.py
 
-from models.design_schema import create_empty_design
+from dataclasses import dataclass, field
 
+@dataclass
 class Context:
-    def __init__(self, data=None):
-        self.data = data or {}
-        self.data["design"] = create_empty_design()
+    data: dict = field(default_factory=dict)
 
-    def update(self, key, value):
+    def set(self, key, value):
         self.data[key] = value
-
-    def update_design(self, key, value):
-        self.data["design"][key] = value
 
     def get(self, key, default=None):
         return self.data.get(key, default)
-
-    def get_design(self):
-        return self.data["design"]
