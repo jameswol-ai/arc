8563489ca37a004e65aaaa5a22998b8820417c23
James Wol <james.wol@petalmail.com>
2026-05-08 05:22:27 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 # IMPORTANT: keep imports at top
-from core.pipeline import run_pipeline
+#from core.pipeline import run_pipeline
 from export.bim_exporter import export_bim
 from structure.eurocode_engine import structural_assessment
 
