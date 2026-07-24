8182a715b2e8343d97a2198e4f896f962ccc7ad5
James Wol <james.wol@petalmail.com>
2026-07-24 21:00:45 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,3 +2,5 @@ from streamlit_vercel import app
 from app import main
 
 app(main)
+<!DOCTYPE html>
+<html><head><meta http-equiv="refresh" content="0; URL='https://arc-eight.streamlit.app'"></head><body></body></html>
